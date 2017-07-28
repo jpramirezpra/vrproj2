8f1ea73ed9aaf399f8e213dc7bae13b94b05f26e
Jose Pablo Ramirez <jp.ramirezpra@gmail.com>
2017-07-27 23:12:35 -0500

    push project 2

---
+++ Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimation : MonoBehaviour {
+    public string AnimationName;
+    public Animator stateMachine;
+
+    private bool created = false;
+    private GvrViewer viewer;
+
+    void Start() {
+        viewer = (GvrViewer)FindObjectOfType(typeof(GvrViewer));
+
+        if (viewer == null)
+        {
+            Debug.LogError("No GvrViewer found. Please drag the GvrViewerMain prefab into the scene.");
+            return;
+        }
+
+        if (viewer != null) {
+            foreach (Camera c in GvrViewer.Instance.GetComponentsInChildren<Camera>()) {
+                //c.enabled = false; // to use the Gvr SDK without adding cameras we have to disable them
+            }
+        }
+
+    }
+
+    void Update() {
+
+        if (viewer == null)
+        {
+            return;
+        }
+
+        if (viewer.Triggered)
+        {
+            if (stateMachine.GetBool(AnimationName))
+            {
+                stateMachine.SetBool(AnimationName, false);
+            } else
+            {
+                stateMachine.SetBool(AnimationName, true);
+            }
+        }
+            
+    }
+
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+GVR = 1.0
+Unity = 5.5.1
+
+Android
+
+Jose Pablo Ramirez
+It took me 4 hours to complete. I made a mistake the first time building it on a wron version of Unity and GVR would not work. I liked modiying the script to work, even though it was thorwing errors for not finding the GVR Viewer.
